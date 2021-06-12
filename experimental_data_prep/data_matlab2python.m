24d20b2e250278e8e6da86115db234aa495bece8
zahasky <czahasky@wisc.edu>
2021-06-12 17:22:17 -0500

    resample_method

--- experimental_data_prep/data_matlab2python.m
+++ experimental_data_prep/data_matlab2python.m
@@ -5,6 +5,8 @@
 clear all
 close all
 
+
+
 % Berea
 % addpath('C:\Users\zahas\Dropbox\Research\Experiment stuff\Data\Stanford_data\BSS_c1\june_17_pet\6_12_single_phase')
 % load('BSS_c1_2ml_2_3mm_vox')
@@ -12,20 +14,30 @@ close all
 % timestep_length = 60;
 % injected pulse volume [mL]
 % inj_pv = 4;
+% Berea porosity
+addpath('B:\Experimental_data\Stanford_data\PET_CT\Berea_sandston_C1\january_16_CT_imbibe\BS_C1_SImb')
+load('dry_average.mat')
+dry = AVG;
+load('full_sat_average.mat')
+wet = AVG;
+vox_size = [0.03125 0.03125 0.125];
+filename = 'berea_porosity_uncoarsened';
+% Calculate porosity
+PET_4D_coarse = (wet-dry)./1000;
 
 % Bentheimer
 % addpath('C:\Users\zahas\Dropbox\Research\Experiment stuff\Data\Stanford_data\Bentheimer_imperial')
-filename = 'Estaillades_3ml_2_3mm_cropped';
-load(filename)
-% % timestep length in seconds
-timestep_length = 85;
-% % injected pulse volume [mL]
-inj_pv = 2;
-% % injected flow rate [mL/min]
-q = 3;
+% filename = 'Estaillades_3ml_2_3mm_cropped';
+% load(filename)
+% % % timestep length in seconds
+% timestep_length = 85;
+% % % injected pulse volume [mL]
+% inj_pv = 2;
+% % % injected flow rate [mL/min]
+% q = 3;
 
 % Image voxel size
-vox_size = [0.2329 0.2329 0.2388];
+% vox_size = [0.2329 0.2329 0.2388];
 
 % Grid definition
 s = size(PET_4D_coarse);
@@ -35,13 +47,13 @@ PET_size = size(PET_4D_coarse);
 
 %% Visual check
 figure
-imagesc(squeeze(PET_4D_coarse(10,:,:, 4)))
+imagesc(squeeze(PET_4D_coarse(10,:,:)))
 axis image
 colorbar
 set(gca,'Ydir','normal')
 
 figure
-imagesc(squeeze(PET_4D_coarse(:,:,7, 4)))
+imagesc(squeeze(PET_4D_coarse(:,:,80)))
 axis image
 % colormap('gray')
 colorbar
@@ -49,9 +61,11 @@ set(gca,'Ydir','normal')
 
 %% WRITE DATA
 % Permute matrix to make easier to load in python
-PET_4D_coarse_python = permute(PET_4D_coarse,[4 3 2 1]);
+PET_4D_coarse_python = permute(PET_4D_coarse,[3 2 1]);
+% PET_4D_coarse_python = permute(PET_4D_coarse,[4 3 2 1]);
 
 % Save to csv
 filename_csv = [filename, '_nan.csv'];
-csvwrite(filename_csv, [PET_4D_coarse_python(:); PET_size(1); PET_size(2); PET_size(3); PET_size(4); timestep_length; q; inj_pv; vox_size(:)])
+% csvwrite(filename_csv, [PET_4D_coarse_python(:); PET_size(1); PET_size(2); PET_size(3); PET_size(4); timestep_length; q; inj_pv; vox_size(:)])
+csvwrite(filename_csv, [PET_4D_coarse_python(:); PET_size(1); PET_size(2); PET_size(3); vox_size(:)])
 
---
+++ experimental_data_prep/exp_porosity_load_plot.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  6 11:10:03 2020
+
+@author: zahas
+"""
+
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.nn.functional import interpolate
+import torch
+# from scipy.interpolate import RegularGridInterpolator
+
+from exp_arrival_3d_functions import plot_2d, exp_arrival_map_function, axial_linear_interp_3D, quantile_calc
+from temporal_moment_testing import mean_exp_bt_map
+
+
+
+# Berea
+data_filename = 'Berea_porosity_uncoarsened_nan'
+
+
+
+# =============================================================================
+# LOAD SELECTED EXAMPLE DATA 
+# =============================================================================
+# Set path to experimental data
+data_dir = os.path.join('.', 'pet_data')
+# data_dir = os.path.join('.')
+
+# Import data
+all_data = np.loadtxt(data_dir + '\\' + data_filename + '.csv', delimiter=',')
+
+# PET_size(1); PET_size(2); PET_size(3); PET_size(4); timestep_length; q; inj_pv; vox_size(:)])
+dz = all_data[-1] # voxel size in z direction (parallel to axis of core)
+dy = all_data[-2] # voxel size in y direction
+dx = all_data[-3] # voxel size in x direction
+nslice = int(all_data[-4])
+print(nslice)
+nrow = int(all_data[-5])
+ncol = int(all_data[-6])
+
+
+# crop off dataset information
+data = all_data[0:-6]
+# reshape from imported column vector to 4D matrix
+data = data.reshape(nrow, ncol, nslice)
+# Flip data to orient top of cores to top of plot
+data = np.flip(data, 0)
+
+
+# Plot at a few timesteps to make sure import is correct
+# plot_2d(data[:,10,:], dz, dy, 'porosity', cmap='viridis')
+plot_2d(data[:,:,0], dx, dy, 'porosity', cmap='viridis')
+
+# remove nans
+data[np.isnan(data)]=0
+data[data<0]=0.001
+data[data>0.5]=0.5
+
+por_tensor = torch.from_numpy(data.copy())
+por_tensor = por_tensor.reshape(1, nrow, ncol, nslice)
+plot_2d(por_tensor[0, :,:,0], dx, dy, 'porosity', cmap='viridis')
+
+# pytorch downsample
+por_tensor_coarse = interpolate(por_tensor, size=[ncol,40], mode='bilinear')
+por_tensor_coarse = por_tensor_coarse.permute(0, 3, 1, 2)
+por_tensor_coarse = interpolate(por_tensor_coarse, size=([20, 20]), mode='bilinear')
+# por_tensor_coarse = interpolate(por_tensor_coarse, size=20, mode='linear')
+
+por_tensor_coarse = por_tensor_coarse.permute(0, 2, 3, 1)
+plot_2d(por_tensor_coarse[0,:,:,0], dx*(nrow/20), dy*(ncol/20), 'porosity', cmap='viridis')
+
+## 3D linear interpolation, this is done after arrival time calculation
+# at_interp_3d, dz_interp = axial_linear_interp_3D(at_diff_norm, dx, dy, dz, 40)
+# plot_2d(at_interp_3d[:,11,:], dz_interp, dy, 'interp arrival time', cmap='bwr')
+
+# save normalized arrival time data
+# save_filename_atdn = 'pet_arrival_time_data'  + '\\' + data_filename[:-12] + '_at.csv'
+# save_data = np.append(at_interp_3d.flatten('C'), [km2])
+# np.savetxt(save_filename_atdn, save_data, delimiter=',')
+
+
+
+
+
+
+
+
+
